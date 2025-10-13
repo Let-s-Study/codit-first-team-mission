b354c148eb35eab00d95fce8787e9e16cfd3ddda
nnnn2001 <hee2ees2@gmail.com>
2025-10-13 13:40:21 +0900

    💄Add styles for header/title:header와 title section에 css 레이아웃 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.scss'
 
 import logoImg from './assets/img/img_logo.png';
 
@@ -7,22 +7,36 @@ function App() {
   const title = "연우의 개발 공장";
 
   return (
-    <div>
-      <img src={logoImg} />
-      <h1>{title}</h1>
-      <div>
-        <button>오늘의 집중</button>
-        <button>홈</button>
-      </div>
-      <div>
-        <h3>현재 시간</h3>
-        <p>{now.toLocaleString('ko-KR', {
-          year: 'numeric',
-          month: '2-digit',
-          day: '2-digit',
-          hour: '2-digit',
-          minute: '2-digit'
-        })}</p>
+    <div className="mainContainer">
+      <img src={logoImg} className="logoImg" />
+      <div className="appContainer">
+        <div className="headerContainer">
+          <div className="titleContainer">
+            <h1 className="title">{title}</h1>
+          </div>
+          <div className="menuContainer">
+            <button>오늘의 집중</button>
+            <button>홈</button>
+          </div>
+        </div>
+        <div className="timeContainer">
+          <h3>현재 시간</h3>
+          <p>{now.toLocaleString('ko-KR', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit'
+          })}</p>
+        </div>
+
+
+        <section className="listSection">
+          <div className="listTitleSection">
+            <h2>오늘의 습관</h2>
+            <button>목록 수정</button>
+          </div>
+        </section>
       </div>
     </div>
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import './styles/reset.css'
-import './styles/main.css'
+import './styles/reset.scss'
+import './styles/main.scss'
 import './index.css'
 import App from './App.jsx'
 
