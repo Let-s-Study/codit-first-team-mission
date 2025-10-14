2c863e9d0291d7041d9e2a8846b8454bd33003cd
nnnn2001 <hee2ees2@gmail.com>
2025-10-14 09:18:53 +0900

    ✨Add TodoItem/ toggle : 습관 목록 구현 후 토글 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
-
+import { React, useState } from 'react';
+import { nanoid } from 'nanoid';
+import { Link } from 'react-router-dom';
 
 import './App.scss'
 
 import logoImg from './assets/img/img_logo.png';
 import arrowRight from './assets/img/ic_arrow_right.png'
 
+
+import { Panel } from './components/Panel/Panel'
+import { TodoItem } from './components/TodoItem/TodoItem'
+
+
 function App() {
   const now = new Date();
   const title = "연우의 개발 공장";
 
+  const [todos, setTodos] = useState([
+    { id: nanoid(), text: '미라클모닝 6시 기상', isDone: true },
+    { id: nanoid(), text: '아침 챙겨 먹기', isDone: true },
+    { id: nanoid(), text: 'React 스터디 책 1챕터 읽기', isDone: false },
+    { id: nanoid(), text: '스트레칭', isDone: false },
+    { id: nanoid(), text: '영양제 챙겨 먹기', isDone: false },
+    { id: nanoid(), text: '사이드 프로젝트', isDone: false },
+    { id: nanoid(), text: '물 2L 먹기', isDone: false },
+  ]);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleClick = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo))
+  }
+
   return (
     <div className="mainContainer">
-      <img src={logoImg} className="logoImg" />
+      <Link to='/'><img src={logoImg} className="logoImg" /></Link>
       <div className="appContainer">
         <div className="headerContainer">
           <div className="titleContainer">
@@ -41,10 +66,27 @@ function App() {
 
 
         <section className="listSection">
-          <div className="listTitleSection">
-            <h2>오늘의 습관</h2>
-            <button>목록 수정</button>
-          </div>
+          <Panel>
+            <div className="listTitleSection">
+              <h2>오늘의 습관</h2>
+              <button onClick={() => setIsOpen(true)}>목록 수정</button>
+
+            </div>
+            {todos.length === 0 ? (
+              <p>아직 습관이 없어요<br />목록 수정을 눌러 습관을 생성해보세요</p>
+            ) : (
+
+              <ul className="todoList">
+                {todos.map((todo) => (
+                  <TodoItem
+                    todo={todo}
+                    onClick={toggleClick}
+                    onDelete={() => onDelete(todo.id)}
+                  />
+                ))}
+              </ul>
+            )}
+          </Panel>
         </section>
       </div>
     </div>
---
+++ src/components/TodoItem/TodoItem.jsx
@@ -0,0 +1,24 @@
+import './TodoItem.scss';
+
+import trashImg from '../../assets/img/btn_determinate.png';
+
+export function TodoItem({ todo, onClick, showDelete = false, onDelete }) {
+  const todoItem = "todo-item"
+  const activeBackground = "active-background"
+  return (
+    <li
+      className={`${todoItem} ${todo.isDone ? activeBackground : ''}`}
+      onClick={() => onClick(todo.id)}>
+      {todo.text}
+      {showDelete && (
+        <img
+          src={trashImg}
+          alt="삭제"
+          className="trashBtn"
+          onClick={(e) => {
+            onDelete(todo.id)
+          }} />
+      )}
+    </li>
+  )
+}
