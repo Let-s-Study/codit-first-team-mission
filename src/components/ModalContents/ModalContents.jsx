c32fe0fec7c6a3e9f204500dbbd8bd682f4df771
nnnn2001 <hee2ees2@gmail.com>
2025-10-15 13:38:27 +0900

    ✨ feat: edit habit feature 습관 수정/삭제/취소 기능 구현

--- src/components/ModalContents/ModalContents.jsx
+++ src/components/ModalContents/ModalContents.jsx
@@ -1,15 +1,52 @@
-import './ModalContents.scss'
+import { React, useState, useEffect } from 'react';
 
-function ModalContents({ onClose }) {
+import './ModalContents.scss'
+import frameimg from '../../assets/img/img_frame.png'
+
+import { TodoItem } from '../TodoItem/TodoItem';
+
+function ModalContents({ todos, onSave, onClose }) {
+
+  const [editTodos, setEditTodos] = useState(todos);
+
+  useEffect(() => {
+    setEditTodos(todos)
+  }, [todos]);
+
+  const handleDelete = (id) => {
+    setEditTodos(prevTodos => prevTodos.filter(todo => todo.id !== id))
+  }
 
   const handleCancel = () => {
+    onClose();
+  }
+
+  const handleSave = () => {
+    onSave(editTodos);
     onClose();
   }
 
   return (
     <div className="listSection">
       <h2>습관 목록</h2>
-      <button onClick={handleCancel}>취소</button>
+
+      {editTodos.length === 0 ? (
+        <p>아직 습관이 없어요<br />목록 수정을 눌러 습관을 생성해보세요</p>
+      ) : (
+        <ul className="todoList">
+          {editTodos.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              showDelete={true}
+              onDelete={() => handleDelete(todo.id)} />
+          ))}
+        </ul>
+      )}
+      <div className="modalBtnWrapper">
+        <button className="cancelBtn" onClick={handleCancel}>취소</button>
+        <button className="modifyBtn" onClick={handleSave}>수정 완료</button>
+      </div>
     </div>
   )
 }
