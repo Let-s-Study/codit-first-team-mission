289d9a1a55c0c1c4199da20394f3fca142e8a0f9
bean-baek <106383499+bean-baek@users.noreply.github.com>
2025-10-16 11:03:18 +0900

    [ Feat/create study ]  헤더 및 라우터 수정, 크리에이트 스터디 수정 (#5)
    * ✨ Feat: CreateStudyPage
    간단한 틀 작업, Router 작업 수정 및 추가
    
    * ✨ Feat: CreateStudyPage
    배경 이미지 선택 작업 초안 & 커밋 버그 수정
    
    * ✨ Feat: CreateStudyPage
    배경 이미지 선택 작업 초안 & 커밋 버그 수정

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,15 @@
 import { Outlet, useLocation } from 'react-router-dom';
-import { Header, CreateStudy } from './Header';
+import { Header } from './Header';
+import { CreateStudyBtn } from './CreateBtn';
+import './Header.module.scss';
 
-export function Layout (){
+export function Layout(){
     const location = useLocation();
     const onHome = location.pathname === "/";
 
     return (
         <div>
-            <Header actions={onHome ? <CreateStudy /> : null} />
+            <Header actions={onHome ? <CreateStudyBtn /> : null} />
             <main>
                 <Outlet />
             </main>
