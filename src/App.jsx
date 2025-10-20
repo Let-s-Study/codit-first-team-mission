faa83da555931822bdeff59502917be116b3aa03
nnnn2001 <hee2ees2@gmail.com>
2025-10-20 18:44:08 +0900

    ✨ feat: add toast message input 3글자 이상 입력 검증

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './styles/global.scss'
 import { Layout } from './components/layout/Layout'
+import { HomePage } from './pages/HomePage/HomePage'
 import { HabitPage } from './pages/HabitPage/HabitPage'
-
 function App() {
   return (
     <Router>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="habit" element={<HabitPage />}>
-          </Route>
+          <Route path="/" element={<HomePage />} />
+          <Route path="habit" element={<HabitPage />} />
         </Route>
       </Routes>
     </Router>
