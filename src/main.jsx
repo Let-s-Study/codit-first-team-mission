d46bec44a8f4cc765b53bf8c660d3780845f7335
bean-baek <suhyunl1989@gmail.com>
2025-10-20 11:39:06 +0900

    fix: before main merge

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
 
 import './styles/reset.scss'
 import './styles/main.scss'
-=======
-import './styles/global.scss'
->>>>>>> feat/Setup-Project
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
 <StrictMode>
     <App />
-<<<<<<< HEAD
-  </StrictMode>,
+  </StrictMode>
 )
-=======
-</StrictMode>,
-)
->>>>>>> feat/Setup-Project
