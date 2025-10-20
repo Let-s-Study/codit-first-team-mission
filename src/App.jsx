d4835b1ffcf770ab505c72b34a32e5bee7c694a6
jyoon00-cloud <ljyoon001126@gmail.com>
2025-10-20 11:50:54 +0900

    Feat: Fix router
    라우터 작업완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './styles/global.scss'
-import { Layout } from './components/layout/Layout'
-import { CreateStudyPage } from './pages/CreateStudy/CreateStudyPage'
-import { HomePage } from './pages/HomePage/HomePage'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./styles/global.scss";
+import { Layout } from "./components/layout/Layout";
+import { CreateStudyPage } from "./pages/CreateStudy/CreateStudyPage";
+import { HomePage } from "./pages/HomePage/HomePage";
+import { FocusPage } from "./pages/FocusPage/FocusPage";
 function App() {
   return (
     <Router>
-        <Routes>
-        <Route element={<Layout/>}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="create" element={<CreateStudyPage/>} />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="create" element={<CreateStudyPage />} />
+          <Route path="focus" element={<FocusPage />} />
         </Route>
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
