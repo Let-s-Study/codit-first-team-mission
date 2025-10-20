be4537cf8edbe6ce0cba9afe23fbcfe3959a9e11
changki <166608656+p-changki@users.noreply.github.com>
2025-10-20 11:29:06 +0900

    [Feat/Home-Page] 홈 페이지 기능 추가 (#14)
    * ✨ feat: HomePage 작성
     홈 페이지 및 스터디 카드 컴포넌트 추가 및 스타일 추가
    
    * ✨ feat: HomePage 구성 요소 및 데이터 정리

--- src/pages/CreateStudy/CreateStudyPage.jsx
+++ src/pages/CreateStudy/CreateStudyPage.jsx
@@ -1,40 +1,25 @@
-import style from './Create.module.scss'
-import {ImageImport} from './ImageImport'
+import style from "./Create.module.scss";
+import { ImageImport } from "./ImageImport";
 
-export function CreateStudyPage(){
-    return (
-        <div className={style.wrap}>
-        <form>
-            <h1>스터디 만들기</h1>
-            <label htmlFor="nickname">닉네임</label>
-            <input
-                type="text"
-                id="name"
-                name="name"
-            />
+export function CreateStudyPage() {
+  return (
+    <div className={style.wrap}>
+      <form>
+        <h1>스터디 만들기</h1>
+        <label htmlFor="nickname">닉네임</label>
+        <input type="text" id="name" name="name" />
 
-            <label htmlFor="description">소개</label>
-            <input
-                type="text"
-                id="description"
-                name="description"
-            />
-            
-            <label htmlFor="background">배경을 선택해주세요</label>
-            <ImageImport />
-            <label htmlFor="password">비밀번호</label>
-            <input 
-                type="text"
-                id="name"
-                name="name"
-                />
-            <label htmlFor="password check">비밀번호</label>
-            <input
-                type="text"
-                id="name"
-                name="name" />
-                <button>만들기</button>
-        </form>
-        </div>
-    );
+        <label htmlFor="description">소개</label>
+        <input type="text" id="description" name="description" />
+
+        <label htmlFor="background">배경을 선택해주세요</label>
+        <ImageImport />
+        <label htmlFor="password">비밀번호</label>
+        <input type="text" id="name" name="name" />
+        <label htmlFor="password check">비밀번호</label>
+        <input type="text" id="name" name="name" />
+        <button>만들기</button>
+      </form>
+    </div>
+  );
 }
