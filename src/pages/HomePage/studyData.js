f87e56d11d266294f97aea04fbd77c8701c87a21
bean-baek <suhyunl1989@gmail.com>
2025-10-24 17:07:35 +0900

    feat: AuthStudyContents

--- src/pages/HomePage/studyData.js
+++ src/pages/HomePage/studyData.js
@@ -1,12 +1,12 @@
-// export const studyList = [
-//   {
-//     id: 1,
-//     title: "K.K.의 UX 스터디",
-//     days: 62,
-//     description: "나비보벳따우",
-//     points: 310,
-//     reactions: { study: 37, fire: 26, heart: 14 },
-//     color: "green",
-//   },
+export const studyList = [
+    {
+        id: 1,
+        title: "K.K.의 UX 스터디",
+        days: 62,
+        description: "나비보벳따우",
+        points: 310,
+        reactions: { study: 37, fire: 26, heart: 14 },
+        color: "green",
+    },
 
-//   ];
+    ];
