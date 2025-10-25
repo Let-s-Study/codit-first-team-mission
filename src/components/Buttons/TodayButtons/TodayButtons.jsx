a60987e513fd5d6df2a737d7e576bc3fdce546f2
jyoon00-cloud <ljyoon001126@gmail.com>
2025-10-25 16:43:00 +0900

    ✨ Feat: Add pw auth 버튼 컴포넌트에 인증모달 추가

--- src/components/Buttons/TodayButtons/TodayButtons.jsx
+++ src/components/Buttons/TodayButtons/TodayButtons.jsx
@@ -3,7 +3,7 @@ import arrowRight from "@/assets/img/ic_arrow_right.png";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "@/context/AuthContext";
-import PaswordModal from "../../Modal/PasswordModal/PasswordModal";
+import PasswordModal from "../../Modal/PasswordModal/PasswordModal";
 
 export function TodayButtons({ value }) {
   const navigate = useNavigate();
@@ -12,16 +12,20 @@ export function TodayButtons({ value }) {
   const [targetPath, setTargetPath] = useState("");
 
   const handleNavigation = (path) => {
+    const relativePath = path.startWith("/") ? path.substring(1) : path;
     if (auth.hasPermission) {
-      navigate(`/study/${auth.studyId}/${path}`);
+      navigate(relativePath);
     } else {
-      setTargetPath(path);
+      setTargetPath(relativePath);
       setIsModalOpen(true);
     }
   };
+
   const handleModalSuccess = () => {
-    navigate(`/study/${auth.studyId}/${targetPath}`);
+    navigate(targetPath);
   };
+
+  const homePath = "/";
 
   const buttonConfig = {
     detail: [
@@ -29,27 +33,27 @@ export function TodayButtons({ value }) {
         오늘의 습관
         <img src={arrowRight} className={styles.arrowIcon} alt="arrow icon" />
       </button>,
-      <button key="detail2">
+      <button key="detail2" onClick={() => handleNavigation("focus")}>
         오늘의 집중
         <img src={arrowRight} className={styles.arrowIcon} alt="arrow icon" />
       </button>,
     ],
     focus: [
-      <button key="focus1">
+      <button key="focus1" onClick={() => handleNavigation("habit")}>
         오늘의 습관
         <img src={arrowRight} className={styles.arrowIcon} alt="arrow icon" />
       </button>,
-      <button key="focus2">
+      <button key="focus2" onClick={() => navigate(homePath)}>
         홈
         <img src={arrowRight} className={styles.arrowIcon} alt="arrow icon" />
       </button>,
     ],
     habit: [
-      <button key="habit">
-        오늘의 습관
+      <button key="habit" onClick={() => navigate(homePath)}>
+        홈
         <img src={arrowRight} className={styles.arrowIcon} alt="arrow icon" />
       </button>,
-      <button key="habit2">
+      <button key="habit2" onClick={() => handleNavigation("focus")}>
         오늘의 집중
         <img src={arrowRight} className={styles.arrowIcon} alt="arrow icon" />
       </button>,
@@ -62,7 +66,8 @@ export function TodayButtons({ value }) {
     <>
       <div className={styles.menuContainer}>{currentButtons}</div>;
       {isModalOpen && (
-        <PaswordModal
+        <PasswordModal
+          isOpen={isModalOpen}
           closeModal={() => setIsModalOpen(false)}
           onSuccess={handleModalSuccess}
         />
