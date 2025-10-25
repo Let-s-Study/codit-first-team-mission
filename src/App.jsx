10a66cd92b9d9c9663e53e8afd2783671cc9b32c
jyoon00-cloud <ljyoon001126@gmail.com>
2025-10-25 18:16:51 +0900

    pw auth 추가 (#29)
    * ✨ Feat: Add Auth
    인증 추가
    
    * ✨ Feat: Add study api
    로그인이랑 포인트 추가
    
    * ✨  Feat:Add pw auth
    비밀번호 검증 추가
    
    * ✨ Feat: Add pw auth
    버튼 컴포넌트에 인증모달 추가
    
    * ✨ Feat: Add auth to HabitPage
    습관 페이지에 인증 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,43 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Layout } from './components/layout/Layout'
-import { HomePage } from './pages/HomePage/HomePage'
-import { HabitPage } from './pages/HabitPage/HabitPage'
-import { FocusPage } from './pages/FocusPage/FocusPage'
-import { CreateStudyPage } from './pages/CreateStudy/CreateStudyPage'
-import { StudyDetailPage } from './pages/StudyDetail/StudyDetailPage'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+  useParams,
+} from "react-router-dom";
+import { Layout } from "./components/layout/Layout";
+import { HomePage } from "./pages/HomePage/HomePage";
+import { HabitPage } from "./pages/HabitPage/HabitPage";
+import { FocusPage } from "./pages/FocusPage/FocusPage";
+import { CreateStudyPage } from "./pages/CreateStudy/CreateStudyPage";
+import { StudyDetailPage } from "./pages/StudyDetail/StudyDetailPage";
+import { AuthProvider } from "./context/AuthContext";
+
+function StudySpecificLayout() {
+  const { studyId } = useParams();
+  if (!studyId) {
+    // studyId가 유효하지 않은경우 어떻게 할지
+    return <div> 잘못된 접근입니다 </div>;
+  }
+  return (
+    <AuthProvider studyId={studyId}>
+      <Outlet />
+    </AuthProvider>
+  );
+}
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<HomePage />} />
-          <Route path="habit" element={<HabitPage />}/>
           <Route path="create" element={<CreateStudyPage />} />
-          <Route path="focus" element={<FocusPage />} />
-          <Route path="habit-detail" element={<StudyDetailPage />} />
+          <Route path="study/:studyId" element={<StudySpecificLayout />}>
+            <Route path="habit" element={<HabitPage />} />
+            <Route path="focus" element={<FocusPage />} />
+            <Route path="detail" element={<StudyDetailPage />} />
+          </Route>
         </Route>
       </Routes>
     </Router>
--- src/components/Buttons/TodayButtons/TodayButtons.jsx
+++ src/components/Buttons/TodayButtons/TodayButtons.jsx
@@ -1,43 +1,77 @@
-import styles from './TodayButtons.module.scss';
-import arrowRight from '@/assets/img/ic_arrow_right.png'
+import styles from "./TodayButtons.module.scss";
+import arrowRight from "@/assets/img/ic_arrow_right.png";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "@/context/AuthContext";
+import PasswordModal from "../../Modal/PasswordModal/PasswordModal";
 
 export function TodayButtons({ value }) {
-    const buttonConfig = {
-        detail: [
-        <button key="detail">
-            오늘의 습관
-            <img src={arrowRight} className={styles.arrowIcon} alt="arrow icon" />
-        </button>,
-        <button key="detail2">
-            오늘의 집중
-            <img src={arrowRight} className={styles.arrowIcon} alt="arrow icon" />
-        </button>
-        ],
-        focus: [
-        <button key="focus1">
-            오늘의 습관
-            <img src={arrowRight} className={styles.arrowIcon} alt="arrow icon" />
-        </button>,
-        <button key="focus2">
-            홈
-            <img src={arrowRight} className={styles.arrowIcon} alt="arrow icon" />
-        </button>
-        ],
-        habit: [
-        <button key="habit">
-            오늘의 습관
-            <img src={arrowRight} className={styles.arrowIcon} alt="arrow icon" />
-        </button>,
-        <button key="habit2">
-            오늘의 집중
-            <img src={arrowRight} className={styles.arrowIcon} alt="arrow icon" />
-        </button>
-        ],
-    };
+  const navigate = useNavigate();
+  const auth = useAuth();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [targetPath, setTargetPath] = useState("");
 
-    const currentButtons = buttonConfig[value]
+  const handleNavigation = (path) => {
+    const relativePath = path.startWith("/") ? path.substring(1) : path;
+    if (auth.hasPermission) {
+      navigate(relativePath);
+    } else {
+      setTargetPath(relativePath);
+      setIsModalOpen(true);
+    }
+  };
 
-    return <div className={styles.menuContainer}>{currentButtons}</div>;
+  const handleModalSuccess = () => {
+    navigate(targetPath);
+  };
+
+  const homePath = "/";
+
+  const buttonConfig = {
+    detail: [
+      <button key="detail" onClick={() => handleNavigation("habit")}>
+        오늘의 습관
+        <img src={arrowRight} className={styles.arrowIcon} alt="arrow icon" />
+      </button>,
+      <button key="detail2" onClick={() => handleNavigation("focus")}>
+        오늘의 집중
+        <img src={arrowRight} className={styles.arrowIcon} alt="arrow icon" />
+      </button>,
+    ],
+    focus: [
+      <button key="focus1" onClick={() => handleNavigation("habit")}>
+        오늘의 습관
+        <img src={arrowRight} className={styles.arrowIcon} alt="arrow icon" />
+      </button>,
+      <button key="focus2" onClick={() => navigate(homePath)}>
+        홈
+        <img src={arrowRight} className={styles.arrowIcon} alt="arrow icon" />
+      </button>,
+    ],
+    habit: [
+      <button key="habit" onClick={() => navigate(homePath)}>
+        홈
+        <img src={arrowRight} className={styles.arrowIcon} alt="arrow icon" />
+      </button>,
+      <button key="habit2" onClick={() => handleNavigation("focus")}>
+        오늘의 집중
+        <img src={arrowRight} className={styles.arrowIcon} alt="arrow icon" />
+      </button>,
+    ],
+  };
+
+  const currentButtons = buttonConfig[value];
+
+  return (
+    <>
+      <div className={styles.menuContainer}>{currentButtons}</div>;
+      {isModalOpen && (
+        <PasswordModal
+          isOpen={isModalOpen}
+          closeModal={() => setIsModalOpen(false)}
+          onSuccess={handleModalSuccess}
+        />
+      )}
+    </>
+  );
 }
-
-
